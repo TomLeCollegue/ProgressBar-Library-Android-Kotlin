b12dfa9033cc559d8f376a419855138522db2f18
Kubasik <tomkubasik.pro@gmail.com>
2021-04-02 11:19:29 +0200

    working on activity

--- app/src/main/java/com/tomlecollegue/testlib/MainActivity.kt
+++ app/src/main/java/com/tomlecollegue/testlib/MainActivity.kt
@@ -2,10 +2,12 @@ package com.tomlecollegue.testlib
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tomlecollegue.progressbars.TestLibrary
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
---
+++ progressbars/src/main/java/com/tomlecollegue/progressbars/TestLibrary.kt
@@ -0,0 +1,9 @@
+package com.tomlecollegue.progressbars
+
+class TestLibrary {
+
+    fun test(arg1 : Int, arg2: Int) : Int{
+        return  arg1 + arg2
+    }
+
+}
